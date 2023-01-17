940f9bd9889a8f243760649b61e0b1fb97c132e5
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-17 12:45:24 +0000

    altered js to reflect html ids as js variables

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,18 @@
 let choicesEl = document.querySelector("#choices");
 let questionTitle = document.querySelector("#question-title");
+let startScreen = document.querySelector("#start-screen");
+let endScreen = document.querySelector("#end-screen");
+let questionPrompts = document.querySelector("#questions");
+let finalScore = document.querySelector("#final-score");
+let initials = document.querySelector("#initials");
+let submit = document.querySelector("#submit");
+let feedback = document.querySelector("#feedback");
+
 
 let questions = [
     { question: "what kind of animal are you?", answers: ["potato", "mammal", "fish", "dinosaur"], correctAnswer: 3 },
-    { question: "what kind of animal are you?", answers: ["potato", "mammal", "fish", "dinosaur"], correctAnswer: 3 },
-
+    { question: "what is the last thing you ate?", answers: ["breakfast", "lunch", "dinner", "junk"], correctAnswer: 1 },
+    { question: "do you understand JavaScript?", answers: ["I've had better luck stapling scrambled eggs to the ceiling", "i am pretending i do to impress my peers", "of course i do my brain is a filofax", "not even remotely, pal"], correctAnswer: 0 }
 ];
 
 let currentQuestion= 0;
